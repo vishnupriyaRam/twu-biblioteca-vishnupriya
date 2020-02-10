a29c94870eb40b629c7b7852286105560a38b743
Vishnupriya Ramanathan <vishnupriya.r@thoughtworks.com>
2020-02-11 02:44:28 +0530

    Refactor remove unused imports from menu test

--- src/test/java/com/twu/biblioteca/model/MenuTest.java
+++ src/test/java/com/twu/biblioteca/model/MenuTest.java
@@ -1,30 +1,10 @@
 package com.twu.biblioteca.model;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MenuTest {
-    private PrintStream originalOut;
-    private ByteArrayOutputStream outContent;
-
-    @BeforeEach
-    void setUp() {
-        outContent = new ByteArrayOutputStream();
-        originalOut = System.out;
-        System.setOut(new PrintStream(outContent));
-    }
-
-    @AfterEach
-    void reset() {
-        System.setOut(originalOut);
-    }
-
     @Test
     void shouldReturnTheListOfMenuItems() {
         Menu menu = new Menu();
