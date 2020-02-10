0e4647779bf6963ff3594501a52db819e37149d0
Vishnupriya Ramanathan <vishnupriya.r@thoughtworks.com>
2020-02-10 15:38:42 +0530

    refactor remove todos from book

--- src/main/java/com/twu/biblioteca/model/Book.java
+++ src/main/java/com/twu/biblioteca/model/Book.java
@@ -6,22 +6,13 @@ import java.util.Objects;
 public class Book {
     public final String title;
     public final String author;
-    public final String year; // TODO - year as a string, sort of weird?
+    public final String year;
 
     public Book(String title, String author, String year) {
         this.title = title;
         this.author = author;
         this.year = year;
     }
-
-    // TODO - new feature - books later than year 2000 (recent books only feature)
-//    public boolean isRecent() {
-//        if(year != null) {
-//            return Integer.parseInt(year) > 2000;
-//        } else {
-//            throw new OperationNotSupportedException();
-//        }
-//    }
 
     public void view() {
         String delimiter = " | ";
