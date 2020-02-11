359422d5545199edbf6cfc6e49cb66272d5d8d32
Vishnupriya Ramanathan <vishnupriya.r@thoughtworks.com>
2020-02-11 16:32:47 +0530

    Todo completed todo

--- src/main/java/com/twu/biblioteca/view/Input.java
+++ src/main/java/com/twu/biblioteca/view/Input.java
@@ -2,7 +2,6 @@ package com.twu.biblioteca.view;
 
 import java.util.Scanner;
 
-// TODO - list down responsibilities of the INPUT class.
 public class Input {
 
     Scanner scanner = new Scanner(System.in);
