d59b5704935b1abf4952f466f3c080c892531d10
Vishnupriya Ramanathan <vishnupriya.r@thoughtworks.com>
2020-02-11 16:58:47 +0530

    Requirement 2.1 Add getDetails to view the details of the movie that was requested

---
+++ src/main/java/com/twu/biblioteca/model/Movie.java
@@ -0,0 +1,20 @@
+package com.twu.biblioteca.model;
+
+public class Movie {
+    private final String title;
+    private final String year;
+    private final String director;
+    private final String rating;
+
+    public Movie(String title, String year, String director, String rating) {
+        this.title = title;
+        this.year = year;
+        this.director = director;
+        this.rating = rating;
+    }
+
+    public String getDetails() {
+        String separator = " | ";
+        return title + separator + year + separator + director + separator + rating;
+    }
+}
---
+++ src/test/java/com/twu/biblioteca/model/MovieTest.java
@@ -0,0 +1,16 @@
+package com.twu.biblioteca.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieTest {
+    @Test
+    void shouldTestIfTheMovieDetailsAreViewable() {
+        Movie movie = new Movie("Forrest Gump", "1994", "Robert Zemeckis", "8.8");
+
+        String expected = "Forrest Gump | 1994 | Robert Zemeckis | 8.8";
+
+        assertEquals(expected, movie.getDetails());
+    }
+}
